ed807c0d5df61e6e028e0b8835ac37d6ef78d3d1
med <mohammed.bennaceur98@gmail.com>
2026-03-28 13:05:31 +0100

    Fix displayvalue column case for PostgreSQL

--- backend/src/Models/Attribute.php
+++ backend/src/Models/Attribute.php
@@ -42,7 +42,7 @@ class Attribute extends Model
                 'id' => $item['id'],
                 'attribute_id' => $attributeId,
                 'value' => $item['value'],
-                'displayValue' => $item['displayValue'] ?? $item['value'],
+                'displayValue' => $item['displayvalue'] ?? $item['value'],
             ];
         }
 
